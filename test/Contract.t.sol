c29f2e687bba624d1698aa19bac8496c2652e6e4
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-06-30 22:46:09 +0200

    init

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -4,9 +4,24 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    uint256 testNumber;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        testNumber = 9000;
+    }
+
+    function testFailNumberIs42() public {
+        assertEq(testNumber, 42);
+    }
+
+    function testCannotSubtract() public {
+        vm.expectRevert(stdError.arithmeticError);
+        testNumber -= 10000;
+    }
+
+    function testCannotSubtractFuzz(uint256 myNumber) public {
+        vm.assume(myNumber > 9000);
+        vm.expectRevert(stdError.arithmeticError);
+        testNumber -= myNumber;
     }
 }
